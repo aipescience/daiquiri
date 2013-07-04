21268d123a5b9de03e143e54807373a1d4a0d03e
Jochen Klar <jklar@aip.de>
2013-07-04 11:22:05 +0200

    csrf element in forms

--- library/Daiquiri/Form/Abstract.php
+++ library/Daiquiri/Form/Abstract.php
@@ -46,6 +46,8 @@ abstract class Daiquiri_Form_Abstract extends Zend_Form {
                     'escape' => false)),
             'Form'
         ));
+
+        $this->addCsrfElement();
     }
 
     /**
@@ -316,14 +318,15 @@ abstract class Daiquiri_Form_Abstract extends Zend_Form {
      * 
      * Adds a hidden tag with a salt for CSRF attack protection.
      */
-    public function createCsrfElement($salt = 'DaiquiriDaiquiriDaiquiri') {
+    public function addCsrfElement() {
         $field = new Zend_Form_Element_Hash('csrf_hash');
         $field->setOptions(array(
             'ignore' => true,
-            'salt' => $salt
+            'salt' => Daiquiri_Config::getInstance()->core->csrfSalt,
+            'decorators' => array('ViewHelper')
         ));
-
-        return $field;
+        $this->addElement($field);
+        return 'csrf_hash';
     }
 
     /**
