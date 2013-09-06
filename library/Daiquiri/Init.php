dbc9eac84b0a451a912ffa058465e6d593daa0dc
Jochen Klar <jklar@aip.de>
2013-09-06 17:16:48 +0200

    setVolatile remove from Init

--- library/Daiquiri/Init.php
+++ library/Daiquiri/Init.php
@@ -191,9 +191,6 @@ class Daiquiri_Init {
         $front->setRequest($request);
 
         $this->_zend_application->bootstrap();
-        
-        // set the auth volatile so no csrf hashes are used
-        Daiquiri_Auth::getInstance()->setVolatile();
     }
 
     private function _initOptions($input_options) {
