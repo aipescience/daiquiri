4cbd9c13d7ca9e0370e79e451b8656cd20a444f8
Jochen Klar <jklar@aip.de>
2013-10-15 14:11:11 +0200

    fix for sessions table

--- modules/auth/models/Resource/Sessions.php
+++ modules/auth/models/Resource/Sessions.php
@@ -38,6 +38,7 @@ class Auth_Model_Resource_Sessions extends Daiquiri_Model_Resource_Table {
 
     public function fetchRows($sqloptions = array()) {
         // get select object
+        $sqloptions['from'][] = 'data';
         $select = $this->getTable()->getSelect();
         $select->where("`data` LIKE '%Zend_Auth%'");
 
@@ -109,4 +110,29 @@ class Auth_Model_Resource_Sessions extends Daiquiri_Model_Resource_Table {
         return $sessions;
     }
 
+    public function countRows(array $sqloptions = null, $tableclass = null) {
+        //get the table
+        $table = $this->getTable($tableclass);
+
+        // create selcet object
+        $select = $table->select();
+        $select->from($table, 'COUNT(*) as count');
+        $select->where("`data` LIKE '%Zend_Auth%'");
+        
+        if ($sqloptions) {
+            if (isset($sqloptions['where'])) {
+                foreach ($sqloptions['where'] as $w) {
+                    $select = $select->where($w);
+                }
+            }
+            if (isset($sqloptions['orWhere'])) {
+                foreach ($sqloptions['orWhere'] as $w) {
+                    $select = $select->orWhere($w);
+                }
+            }
+        }
+
+        // query database and return
+        return (int) $table->fetchRow($select)->count;
+    }
 }
