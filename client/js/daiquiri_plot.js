3ee03fca298ef0beb0a6c0b16eb8f74343d199b3
Jochen Klar <jklar@aip.de>
2013-12-12 13:48:30 +0100

    some fix and refactoring for plot

--- client/js/daiquiri_plot.js
+++ client/js/daiquiri_plot.js
@@ -25,191 +25,186 @@
  *
  */
 
-var _daiquiri_plot = {
-    defaults: {
 
-    },
-    items: {}
-}
+// daiquiri namespace
+var daiquiri = daiquiri || {};
+daiquiri.plot = {};
 
-function Daiquiri_Plot(container, opt) {
-    // set state
-    this.container = container;
-    this.opt = opt;
- 
-    this.ajaxError = function (jqXHR, textStatus, errorThrown) {
-        var message = 'Error with ajax call';
-        console.log(message);
-        console.log({
-            'status': jqXHR.status,
-            'statusText': jqXHR.statusText,
-            'responseText': jqXHR.responseText
-        });
-    };
-    
-    this.displayForm = function () {
-        var self = this;
-        
-        // get colums by ajax call 
-        $.ajax({
-            url: self.opt.colsurl,
-            type: 'GET',
-            dataType: 'json',
-            headers: {
-                'Accept': 'application/json'
-            },
-            data: {
-                'db': self.opt.params.db,
-                'table': self.opt.params.table
-            },
-            error: self.ajaxError,
-            success: function (json) {
-                var selectOptions = '';
-                for (var i = 0; i < json.cols.length; i++) {
-                    var col = json.cols[i].name;
-                    selectOptions += '<option label="' + col + '"value="'+ col +'">' + col + '</option>'
-                }
-                
-                var html = '<h4>Select Colums for plot</h4>';
-                html += '<p>';
-                html += '<label for="xCol" class="required">Column for X axis:</label>'
-                html += '<select id="xCol" name="xCol" value="1">';
-                html += selectOptions;
-                html += '</select>';
-                html += '<label for="yCol" class="required">Column for Y axis:</label>'
-                html += '<select id="yCol" name="yCol" value="2">';
-                html += selectOptions;
-                html += '</select>';
-                html += '<label for="number" class="required">Number of rows to plot:</label>'
-                html += '<input type="text" name="number" id="number" value="1000">'
-                html += '</p><p>';
-                html += '<button id="submitplot" value="Create Plot" class="btn btn-primary">Create Plot</button>'
-                html += '</p>'
-
-                $('<div/>',{
-                    'html' : html
-                }).appendTo(self.opt.form);
-                
-                if (json.cols.length > 2) {
-                    $("#xCol option[value='" + json.cols[1].name + "']").attr('selected',true);
-                    $("#yCol option[value='" + json.cols[2].name + "']").attr('selected',true);
-                } else if (json.cols.length == 2) {
-                    $("#yCol option[value='" + json.cols[1].name + "']").attr('selected',true);
-                }
-                
-                $('#submitplot').click(function(){
-                    self.displayPlot($('#xCol').val(),$('#yCol').val(),$('#number').val());
-                });
-            }
-        });
-    };
-    this.displayPlot = function (xCol, yCol, number) {
-        var self = this;
-        // get colums by ajax call 
-        $.ajax({
-            url: self.opt.rowsurl,
-            type: 'GET',
-            dataType: 'json',
-            headers: {
-                'Accept': 'application/json'
-            },
-            data: {
-                'db': self.opt.params.db,
-                'table': self.opt.params.table,
-                'nrows': number,
-                'cols': xCol + ',' + yCol
-            },
-            error: self.ajaxError,
-            success: function (json) {
-                var x,y;
-                var plotData = [];
-
-                // gather data for plot
-                for (var i = 0; i < json.rows.length; i++) {
-                    x = json.rows[i]['cell'][0];
-                    if (json.rows[i]['cell'].length > 1) {
-                        y = json.rows[i]['cell'][1];
-                    } else {
-                        y = x;
-                    }
-                    plotData.push([x,y]);
-                }
-
-                var series = {
-                    color: "#900",
-                    data: plotData
-                }
-  
-                var opt = {
-                    color: "#900",
-                    lines: {
-                        show: false
-                    },
-                    points: {
-                        radius: 0.5,
-                        show: true,
-                        fill: true,
-                        fillColor: "#900"
-                
-                    },
-                    shadowSize: 0,
-                    xaxis: {
-                        tickFormatter: self.tickFormatter
-                    },
-                    yaxis: {
-                        tickFormatter: self.tickFormatter
-                    }
-                };
-
-                // draw plot
-                $.plot(self.container, [series], opt);
-            }
-        });
-    };
-    
-    /**
-     * Tick formatter to format ticks.
-     */
-    this.tickFormatter = function(val, axis) {
-        if (val > 1000) {
-            var exp = Math.floor(Math.log(val) / Math.LN10);
-            var man = val / Math.pow(10,exp);
-            return (man).toFixed(3) + "E" + exp;
-        } else {
-            return val.toFixed(axis.tickDecimals);
-        }
-    }
-    
-    /**
-     * Resets the container to the inital state
-     */
-    this.reset = function() {       
-        this.container.children().remove();
-        this.opt.form.children().remove();
-    };
-}
-
-// main plugin
+/**
+ * jquery plugin to insert a plot area in a given jquery selection
+ */
 (function($){
     $.fn.extend({ 
         daiquiri_plot: function(opt) {
-            
-            // apply default options
-            opt = $.extend(_daiquiri_plot.defaults, opt);
-
             return this.each(function() {
                 var id = $(this).attr('id');
 
                 // check if plot is already set
-                if (_daiquiri_plot.items[id] == undefined) {
-                    _daiquiri_plot.items[id] = new Daiquiri_Plot($(this),opt);
+                if (daiquiri.plot.items[id] == undefined) {
+                    daiquiri.plot.items[id] = new daiquiri.plot.Plot($(this),opt);
                 } else {
-                    _daiquiri_plot.items[id].reset();
-                    _daiquiri_plot.items[id].opt = opt;
+                    daiquiri.plot.items[id].reset();
+                    daiquiri.plot.items[id].opt = opt;
                 }
                 
-                _daiquiri_plot.items[id].displayForm();
+                daiquiri.plot.items[id].displayForm();
             });  
         }
     });
 })(jQuery);
+
+/**
+ * Object to hold the CodeMirror instances.
+ */
+daiquiri.plot.items = {};
+
+/**
+ * Constructor-like function for the Plot class. 
+ */
+daiquiri.plot.Plot = function(container, opt) {
+    // set state
+    this.container = container;
+    this.opt = opt;
+}
+
+daiquiri.plot.Plot.prototype.displayForm = function () {
+    var self = this;
+    
+    // get colums by ajax call 
+    $.ajax({
+        url: self.opt.colsurl,
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Accept': 'application/json'
+        },
+        data: {
+            'db': self.opt.params.db,
+            'table': self.opt.params.table
+        },
+        error: self.ajaxError,
+        success: function (json) {
+            var selectOptions = '';
+            for (var i = 0; i < json.cols.length; i++) {
+                var col = json.cols[i].name;
+                selectOptions += '<option label="' + col + '"value="'+ col +'">' + col + '</option>'
+            }
+            
+            var html = '<h4>Select Colums for plot</h4>';
+            html += '<p>';
+            html += '<label for="xCol" class="required">Column for X axis:</label>'
+            html += '<select id="xCol" name="xCol" value="1">';
+            html += selectOptions;
+            html += '</select>';
+            html += '<label for="yCol" class="required">Column for Y axis:</label>'
+            html += '<select id="yCol" name="yCol" value="2">';
+            html += selectOptions;
+            html += '</select>';
+            html += '<label for="number" class="required">Number of rows to plot:</label>'
+            html += '<input type="text" name="number" id="number" value="1000">'
+            html += '</p><p>';
+            html += '<button id="submitplot" value="Create Plot" class="btn btn-primary">Create Plot</button>'
+            html += '</p>'
+
+            $('<div/>',{
+                'html' : html
+            }).appendTo(self.opt.form);
+            
+            if (json.cols.length > 2) {
+                $("#xCol option[value='" + json.cols[1].name + "']").attr('selected',true);
+                $("#yCol option[value='" + json.cols[2].name + "']").attr('selected',true);
+            } else if (json.cols.length == 2) {
+                $("#yCol option[value='" + json.cols[1].name + "']").attr('selected',true);
+            }
+            
+            $('#submitplot').click(function(){
+                self.displayPlot($('#xCol').val(),$('#yCol').val(),$('#number').val());
+            });
+        }
+    });
+};
+
+daiquiri.plot.Plot.prototype.displayPlot = function (xCol, yCol, number) {
+    var self = this;
+    // get colums by ajax call 
+    $.ajax({
+        url: self.opt.rowsurl,
+        type: 'GET',
+        dataType: 'json',
+        headers: {
+            'Accept': 'application/json'
+        },
+        data: {
+            'db': self.opt.params.db,
+            'table': self.opt.params.table,
+            'nrows': number,
+            'cols': 'row_id,' + xCol + ',' + yCol
+        },
+        error: self.ajaxError,
+        success: function (json) {
+            var x,y;
+            var plotData = [];
+
+            // gather data for plot
+            for (var i = 0; i < json.rows.length; i++) {
+                x = json.rows[i]['cell'][0];
+                if (json.rows[i]['cell'].length > 1) {
+                    y = json.rows[i]['cell'][1];
+                } else {
+                    y = x;
+                }
+                plotData.push([x,y]);
+            }
+
+            var series = {
+                color: "#900",
+                data: plotData
+            }
+  
+            var opt = {
+                color: "#900",
+                lines: {
+                    show: false
+                },
+                points: {
+                    radius: 0.5,
+                    show: true,
+                    fill: true,
+                    fillColor: "#900"
+            
+                },
+                shadowSize: 0,
+                xaxis: {
+                    tickFormatter: self.tickFormatter
+                },
+                yaxis: {
+                    tickFormatter: self.tickFormatter
+                }
+            };
+
+            // draw plot
+            $.plot(self.container, [series], opt);
+        }
+    });
+};
+    
+/**
+ * Tick formatter to format ticks.
+ */
+daiquiri.plot.Plot.prototype.tickFormatter = function(val, axis) {
+    if (val > 1000) {
+        var exp = Math.floor(Math.log(val) / Math.LN10);
+        var man = val / Math.pow(10,exp);
+        return (man).toFixed(3) + "E" + exp;
+    } else {
+        return val.toFixed(axis.tickDecimals);
+    }
+};
+    
+/**
+ * Resets the container to the inital state
+ */
+daiquiri.plot.Plot.prototype.reset = function() {       
+    this.container.children().remove();
+    this.opt.form.children().remove();
+};
