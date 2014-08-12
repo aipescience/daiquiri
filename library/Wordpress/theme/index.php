17a8e3f67030376dc65fc7e636ab62e79b6f6eda
Jochen Klar <jklar@aip.de>
2014-08-12 12:32:41 +0200

    further improvement for wordpress theme

--- library/Wordpress/theme/index.php
+++ library/Wordpress/theme/index.php
@@ -24,12 +24,12 @@
 <div id="wp-content" class="row">
     <div class="span9 main">
         <h2><?php wp_title(''); ?></h2>
-        
+
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="post">
-                <h2>
+                <h3>
                     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                </h2>
+                </h3>
 
                 <?php the_content(); ?>
 
