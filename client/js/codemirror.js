382afa459ba2cc1ccda2815de0e93f1fda04f21d
Jochen Klar <jklar@aip.de>
2015-01-12 18:23:53 +0100

    codemirror files added

---
+++ client/js/codemirror.js
@@ -0,0 +1,54 @@
+/*  
+ *  Copyright (c) 2012-2014 Jochen S. Klar <jklar@aip.de>,
+ *                           Adrian M. Partl <apartl@aip.de>, 
+ *                           AIP E-Science (www.aip.de)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+angular.module('codemirror', [])
+
+.directive('codemirror', ['CodemirrorService',function(CodemirrorService) {
+    return {
+        restrict: 'C', // only for the .codemirror class
+        link: function(scope, elements) {
+            elements.each(function(key,element) {
+                CodemirrorService.elements[key] = CodeMirror.fromTextArea(element, {
+                    mode: 'text/x-mysql',
+                    indentWithTabs: false,
+                    smartIndent: true,
+                    matchBrackets : true,
+                    lineNumbers: true,
+                    lineWrapping: true,
+                    autofocus: true
+                });
+                CodemirrorService.elements[key].setSize(angular.element(element).width(),null);
+            });
+        }
+    };
+}])
+
+.factory('CodemirrorService', [function() {
+    var elements = {};
+
+    return {
+        elements: elements,
+        save: function() {
+            angular.forEach(elements,function(object, key) {
+                console.log(object);
+                object.save();
+            });
+        }
+    };
+}])
