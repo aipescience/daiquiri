a33dd131fbcbca73235fbe39aec35ef780703589
Jochen Klar <jklar@aip.de>
2015-06-29 11:40:02 +0200

    wide pages for wordpress

---
+++ library/Wordpress/theme/page-wide.php
@@ -0,0 +1,53 @@
+<?php
+/*
+Template Name: Wide
+*/
+?>
+<?php
+/*
+ *  Copyright (c) 2012-2015  Jochen S. Klar <jklar@aip.de>,
+ *                           Adrian M. Partl <apartl@aip.de>,
+ *                           AIP E-Science (www.aip.de)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<?php get_header(); ?>
+
+<div id="wp-content" class="row">
+    <div class="span12 main">
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="post">
+                <h2>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+
+                <?php the_content(); ?>
+
+                <?php edit_post_link('Edit Page', '<div>', '</div>'); ?>
+
+                <?php if (comments_open(get_the_ID())): ?>
+                    <?php comments_template() ?>
+                <?php endif ?>
+
+                <p align="center"><?php posts_nav_link(); ?></p>
+            </div>
+        <?php endwhile; else: ?>
+            <p>Sorry, no page found.</p>
+        <?php endif; ?>
+    </div>
+</div>
+
+<?php get_footer(); ?>
