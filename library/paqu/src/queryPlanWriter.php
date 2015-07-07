62fee5a53233d82cf8a8fd36dec572dbfba70baf
Jochen Klar <jklar@aip.de>
2015-07-07 17:24:49 +0200

    fix for non parallel (i.e. userdb) queries in the query plan writer

--- library/paqu/src/queryPlanWriter.php
+++ library/paqu/src/queryPlanWriter.php
@@ -38,7 +38,7 @@ include_once 'shard-query-parallel.php';
  * The functions in this file take the optimised query plan produced by shard-query-parallel
  * and rewrite it into a query plan using the MySQL spider shard UDF and our custom defined
  * parallelisation commands.
- * 
+ *
  * These functions also wrap the shard-query-parallel tools and drive the whole parallel
  * optimisation process.
  */
@@ -49,7 +49,7 @@ include_once 'shard-query-parallel.php';
  * @param query SQL query that will be parallelised
  * @param headNodeTables list of tables that are completely located on the head node and are not sharded
  * @return the raw sharded queries to be run on the coordination and shard nodes
- * 
+ *
  * These function will create a parallel query plan from an input SQL statement
  * using shard-query-parallel. It will identify any implicit joins, properly separate
  * them into distinct queries and assign them to temporary tables. Further any aggregate
@@ -60,19 +60,19 @@ function PHPSQLprepareQuery($query, $headNodeTables = array()) {
     $shard_query = new ShardQuery();
     $shard_query->verbose = false;
     $shard_query->headNodeTables = $headNodeTables;
-    
+
     $shard_query->process_sql($query);
 
     if(!empty($shard_query->errors)) {
-    	echo "An Error occured:\n\n";
-    	foreach($shard_query->errors as $key => $error) {
+      echo "An Error occured:\n\n";
+      foreach($shard_query->errors as $key => $error) {
         if(array_key_exists("error_clause", $error)) {
-    	    echo $key . ": clause: " . $error["error_clause"] . " error: " . $error["error_reason"] . "\n";
+          echo $key . ": clause: " . $error["error_clause"] . " error: " . $error["error_reason"] . "\n";
         } else {
           echo $key . ": clause: " . $error[0] . " error: " . $error[1] . "\n";
         }
-  	  }
-	
+      }
+
       throw new Exception('SQL Parse Error');
     }
 
@@ -80,38 +80,38 @@ function PHPSQLprepareQuery($query, $headNodeTables = array()) {
     #parallel only if there are other tables than temporary (i.e. dependent) ones involved
     $parallel = false;
     if(!empty($shard_query->parsedCopy['FROM'])) {
-	    foreach($shard_query->parsedCopy['FROM'] as $fromNode) {
-	    	if($fromNode['table'] != 'DEPENDENT-SUBQUERY') {
-	  	    $parallel = true;
+      foreach($shard_query->parsedCopy['FROM'] as $fromNode) {
+        if($fromNode['table'] != 'DEPENDENT-SUBQUERY') {
+          $parallel = true;
 
-	        //check if this table is only available on the head node
-	        //if yes, donot execute the query in parallel
-	 	 	 $found = false;
-	 	 	 $tableName = str_replace("`", "", $fromNode['table']);
-	 	 	 $posDot = strpos($tableName, ".");
-			 foreach($headNodeTables as $headNodeTable) {
-			 	$posTable = strpos($tableName, $headNodeTable);
-			 	if($posTable !== false) {
-			 		if($posDot === false) {
-			 			if(strlen($tableName) === strlen($headNodeTable)) {
-			 				$found = true;
-			 			}
-			 		} else {
-			 			if(strlen($tableName) - $posDot - 1 === strlen($headNodeTable)) {
-			 				$found = true;
-			 			} else if ($posTable == 0 && strlen($headNodeTable) == $posDot) {
-			 				$found = true;
-			 			}
-			 		}
+          //check if this table is only available on the head node
+          //if yes, donot execute the query in parallel
+       $found = false;
+       $tableName = str_replace("`", "", $fromNode['table']);
+       $posDot = strpos($tableName, ".");
+       foreach($headNodeTables as $headNodeTable) {
+        $posTable = strpos($tableName, $headNodeTable);
+        if($posTable !== false) {
+          if($posDot === false) {
+            if(strlen($tableName) === strlen($headNodeTable)) {
+              $found = true;
+            }
+          } else {
+            if(strlen($tableName) - $posDot - 1 === strlen($headNodeTable)) {
+              $found = true;
+            } else if ($posTable == 0 && strlen($headNodeTable) == $posDot) {
+              $found = true;
+            }
+          }
 
-			 		if($found === true) {
-			            $parallel = false;
-						break;
-			 		}
-			 	}
-			 }
-	    	}
-	    }
+          if($found === true) {
+                  $parallel = false;
+            break;
+          }
+        }
+       }
+        }
+      }
     }
 
     $shard_query->subqueries[$shard_query->table_name] = $shard_query->shard_sql;
@@ -121,13 +121,13 @@ function PHPSQLprepareQuery($query, $headNodeTables = array()) {
 }
 
 /**
- * @brief Generates a set of statements using the parallel query function to 
+ * @brief Generates a set of statements using the parallel query function to
  * @param shard_query the sharded queries produced by the parallel query optimiser
  * @param resultTable the name of the result table to write the results into
  * @param addRowNumber add row numbers to the final result table
  * @param aggregateDist flag that defines whether aggregate results should be treated as distinct
  * @return parallel query plan using the parallel query functions
- * 
+ *
  * This function takes a query plan and produces a query plan using our parallel query functions. These
  * need to be unfortunately processed by the parallelQuery class, since MySQL stored procedures are not
  * doing a great job with dynamic SQL. However this gives the opportunity to actually intersect the query
@@ -138,42 +138,44 @@ function PHPSQLqueryPlanWriter($shard_query, $resultTable, $addRowNumber = false
     $dropTables = array();
 
     foreach($shard_query->subqueries as $key => $query) {
-    	if($query['parallel'] === true) {
+      if($query['parallel'] === true) {
         $query[0] = str_replace("\'", "'", $query[0]);
         $query[0] = str_replace("'", "\'", $query[0]);
 
-  	    $paraQuery = "CALL paquExec('" . str_replace("\n", " ", $query[0]) . "', '" . $key . "')";
-  	    
-  	    $dropTableHead = "CALL paquDropTmp('" . $key . "')";
+        $paraQuery = "CALL paquExec('" . str_replace("\n", " ", $query[0]) . "', '" . $key . "')";
 
-  	    array_push($commandArray, $paraQuery);
-  	    array_push($dropTables, $dropTableHead);
-    	} else {
+        $dropTableHead = "CALL paquDropTmp('" . $key . "')";
+
+        array_push($commandArray, $paraQuery);
+        array_push($dropTables, $dropTableHead);
+      } else {
         #remove any LIMIT clause, that might interfere
         #if this limit is part of a subquery, there must be a closing parenthesis at pos > limit_pos
         $tmpPos = strrpos($query[0], "LIMIT");
         $tmpParPos = strrpos($query[0], ")");
-        if($tmpPos === false || $tmpPos < $tmpParPos) {
-            $tmpPos = strlen($query[0]);
+        if ($tmpPos === false || $tmpPos < $tmpParPos) {
+          $tmpPos = strlen($query[0]);
         }
         $limitFreeQuery = substr($query[0], 0, $tmpPos);
 
-  	    $hostTableCreateQuery = "CREATE DATABASE IF NOT EXISTS spider_tmp_shard; USE spider_tmp_shard; CREATE TABLE spider_tmp_shard." . $key . " ENGINE=MyISAM " . $limitFreeQuery . " LIMIT 0";
-  	    $shardActualQuery = "USE spider_tmp_shard; INSERT INTO spider_tmp_shard." . $key . " ". $query[0]  . ";";
-        $shardActualQuery .= "\nCALL paquLinkTmp('" . $key . "')";
-  	    //$dropTableHead = "DROP TABLE spider_tmp_shard." . $key ;
+        $hostTableCreateQuery = "CREATE DATABASE IF NOT EXISTS spider_tmp_shard; USE spider_tmp_shard; CREATE TABLE spider_tmp_shard." . $key . " ENGINE=MyISAM " . $limitFreeQuery . " LIMIT 0";
+
+        $shardActualQuery = "USE spider_tmp_shard; INSERT INTO spider_tmp_shard." . $key . " ". $query[0]  . ";";
+
+        $linkTableHead = "CALL paquLinkTmp('" . $key . "')";
+
         $dropTableHead = "CALL paquDropTmp('" . $key . "')";
 
-
-      	array_push($commandArray, $hostTableCreateQuery);
-  	    array_push($commandArray, $shardActualQuery);
-  	    array_push($dropTables, $dropTableHead);
-    	}
+        array_push($commandArray, $hostTableCreateQuery);
+        array_push($commandArray, $shardActualQuery);
+        array_push($commandArray, $linkTableHead);
+        array_push($dropTables, $dropTableHead);
+      }
     }
 
     #remove the last drop table since we want to keep the results for further use
     $tmpQuery = array_pop($dropTables);
-    
+
     #gather the result into table
     array_push($dropTables, PHPSQLaggregateResult($shard_query, $resultTable, $addRowNumber, $aggregateDist));
     array_push($dropTables, $tmpQuery);
@@ -188,17 +190,17 @@ function PHPSQLqueryPlanWriter($shard_query, $resultTable, $addRowNumber = false
  * @param addRowNumber add row numbers to the final result table
  * @param aggregateDist flag that defines whether aggregate results should be treated as distinct
  * @return Coordination/Aggregation SELECT statement with DISTINCT clause added
- * 
- * 
+ *
+ *
  */
 function PHPSQLaggregateResult($shard_query, $resultTable, $addRowNumber = false, $aggregateDist = true) {
     #add distinct clause after SELECT in corrdination query if user asks for it
     if($aggregateDist == true) {
-    	if(strpos($shard_query->coord_sql, "DISTINCT") === false) {
-  	    $shard_query->coord_sql = str_replace("SELECT", "SELECT DISTINCT", $shard_query->coord_sql);
-    	}
+      if(strpos($shard_query->coord_sql, "DISTINCT") === false) {
+        $shard_query->coord_sql = str_replace("SELECT", "SELECT DISTINCT", $shard_query->coord_sql);
+      }
     }
-    
+
     if($addRowNumber === true) {
       if(strpos($shard_query->coord_sql, "DISTINCT") === false) {
         $shard_query->coord_sql = str_replace("SELECT", "SELECT @i:=@i+1 AS `row_id`, ", $shard_query->coord_sql);
