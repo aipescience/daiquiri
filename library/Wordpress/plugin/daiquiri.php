c88aff41fab1d57e186cb8d77b22aac88854d004
Jochen Klar <jklar@aip.de>
2015-08-14 14:29:40 +0200

    small fix

--- library/Wordpress/plugin/daiquiri.php
+++ library/Wordpress/plugin/daiquiri.php
@@ -103,9 +103,9 @@ function daiquiri_auto_login()
                 $wpUser['role'] = 'administrator';
             } else if ($daiquiriUser['role'] === 'manager') {
                 $wpUser['role'] = 'editor';
-            } else if (defined(DAIQUIRI_AUTHOR_ROLE) && $daiquiriUser['role'] === DAIQUIRI_AUTHOR_ROLE) {
+            } else if (defined('DAIQUIRI_AUTHOR_ROLE') && $daiquiriUser['role'] === DAIQUIRI_AUTHOR_ROLE) {
                 $wpUser['role'] = 'author';
-            } else if (defined(DAIQUIRI_CONTRIBUTOR_ROLE) && $daiquiriUser['role'] === DAIQUIRI_CONTRIBUTOR_ROLE) {
+            } else if (defined('DAIQUIRI_CONTRIBUTOR_ROLE') && $daiquiriUser['role'] === DAIQUIRI_CONTRIBUTOR_ROLE) {
                 $wpUser['role'] = 'contributor';
             } else {
                 $wpUser['role'] = 'subscriber';
